802f178f81a55fbf1160c6dc9d9aeaa53c11944d
Pablo Violi <violi.pablo@gmail.com>
2018-09-09 22:57:03 -0300

    <Funcion de Registro por Procedure>

--- application/controllers/RegistrarSocio.php
+++ application/controllers/RegistrarSocio.php
@@ -13,17 +13,29 @@ class RegistrarSocio extends CI_Controller {
     }
     
     public function Asociar() {
+        $this->load->model("dao/LoginDao");
         $nombre = $this->input->post('nombre');
         $apellido = $this->input->post('apellido');
-        $direc = $this->input->post('direc');
+        $direccion = $this->input->post('direc');
         $telefono = $this->input->post('tel');
         $email = $this->input->post('email');
-        
         $user = $this->input->post('user');
         $pass = $this->input->post('pass');
         
-        
-    }
+        $registrar = $this->LoginDao->registrarUsuario($user,$pass,$nombre,$apellido,$direccion,$telefono,$email);
+         
+        if ($registrar===0) {
+            $_SESSION['errorRegistro'] = "Error al registrarse. Intente nuevamente";
+            $this->load->view("RegistrarSocio");
+            $this->load->view("footer");
+        }
+        else {
+            $_SESSION['RegistroOk'] = "Se ha registrado correctamente";
+            $this->load->view('index');
+            $this->load->view("footer");
+        }
+        }
+    
     
     public function TraerUsuarios() {
         
